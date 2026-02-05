50f21b66c974aff5e8ae30ac673b551df0084af5
Shahil Sinha <dopedits50@gmail.com>
2026-02-05 11:09:27 +0530

    add questions module

---
+++ questions.py
@@ -0,0 +1,11 @@
+# questions.py
+questions = [
+    {
+        "question": "What is the capital of India?",
+        "answer": "Delhi"
+    },
+    {
+        "question": "Which language is used for backend?",
+        "answer": "Python"
+    }
+]
